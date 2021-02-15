da88e314d8dfa0c01e207dbb07624a521d073ccd
Rod Davison <rod@exgnosis.ca>
2021-02-15 16:36:36 -0500

    Modified code for code/terraform/02-intro-to-terraform-syntax

--- code/terraform/02-intro-to-terraform-syntax/one-webserver/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/one-webserver/main.tf
@@ -1,12 +1,7 @@
-terraform {
-  required_version = ">= 0.12, <= 0.14.6"
-}
 
 provider "aws" {
   region = "us-east-2"
 
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
 }
 
 resource "aws_instance" "example" {
